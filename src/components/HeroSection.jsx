dcec66ecd66af2f039267fa865e6d31d5fd901b5
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-08-20 23:38:30 +0500

    commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ function HeroSection() {
                 <h2 className="text-[48px] font-extrabold ">Har lahza uchun ideal kofeni toping.</h2>
                 <p className="text-[20px] font-normal text-[#403937] ">Istalgan joyda va har qanday vaqtda Coffee Delivery bilan sevimli kofeingizdan bahramand bo'ling.</p>
             </div>
-            <div className="">
+            <div className="flex flex-wrap gap-[20px]">
                 {
                     heroData.map((item)=>(
                         <li key={item.id} className="flex" >
